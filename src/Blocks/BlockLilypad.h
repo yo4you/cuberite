6f8d647c39341a9bb042b0cecb945a3c952e82f4
yo4you <yo4you.nl@gmail.com>
2015-08-02 22:49:09 +0200

    Update BlockLilypad.h

--- src/Blocks/BlockLilypad.h
+++ src/Blocks/BlockLilypad.h
@@ -17,7 +17,12 @@ public:
 		super(a_BlockType)
 	{
 	}
-
+	
+	virtual bool CanBeAt(cChunkInterface & a_ChunkInterface, int a_RelX, int a_RelY, int a_RelZ, const cChunk & a_Chunk) override
+	{
+		return ((a_RelY > 0) && ((a_Chunk.GetBlock(a_RelX, a_RelY - 1, a_RelZ) == E_BLOCK_STATIONARY_WATER) || (a_Chunk.GetBlock(a_RelX, a_RelY - 1, a_RelZ) == E_BLOCK_WATER)));
+	}
+	
 	virtual ColourID GetMapBaseColourID(NIBBLETYPE a_Meta) override
 	{
 		UNUSED(a_Meta);
